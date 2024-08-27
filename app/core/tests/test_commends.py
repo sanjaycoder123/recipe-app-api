e13be064e733827892a1344c21cd4815da5cd461
Sanjay Bhattacharjee <sanjay.bhattacharjee1993@gmail.com>
2024-08-27 22:15:28 +0530

    module updated for action

--- app/core/tests/test_commends.py
+++ app/core/tests/test_commends.py
@@ -12,24 +12,17 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
     """Test Commands."""
-
     def test_wait_for_db_ready(self,patched_check):
         """Test wait for database if database ready"""
         patched_check.return_value= True
-        
-        call_command('wait_for_db')
-        
+        call_command('wait_for_db')        
         patched_check.assert_called_once_with(databases=['default'])
 
-
     @patch('time.sleep')
-    def test_wait_for_db_delay(self,patched_sleep,patched_check):
+    def test_wait_for_db_delay(self , patched_sleep , patched_check):
         """Test wait for database when getting operationcal error"""
         patched_check.side_effect = [Pycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]    
-        
         call_command('wait_for_db')
-
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-    
